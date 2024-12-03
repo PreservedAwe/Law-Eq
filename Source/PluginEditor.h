8d63a3a4df0716a8cc0f43ce792cdee720c99f7d
PreservedAwe <154258651+PreservedAwe@users.noreply.github.com>
2024-12-03 14:57:32 -0500

    Made custom LookAndFeel struct for styling components with basic behaviour

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -10,15 +10,36 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include "CustomLookAndFeel.h"
 
 //---------------------Custom GUI Components--------------------------------------------------
+
 struct CustomRotarySlider : juce::Slider
 {
     CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
         juce::Slider::TextEntryBoxPosition::NoTextBox)
     {
-
+        setLookAndFeel(&customLnf);
     }
+
+    ~CustomRotarySlider() 
+    {
+        setLookAndFeel(nullptr);
+    }
+
+    void paint(juce::Graphics& g) override 
+    {
+        auto startAngle = juce::degreesToRadians(180.f + 45.f);
+        auto endAngle = juce::degreesToRadians(180.f - 45.f + (360.f));
+        auto range = getRange();
+        auto sliderProportional = juce::jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0);
+        auto sliderBounds = getLocalBounds();
+
+        getLookAndFeel().drawRotarySlider(g, sliderBounds.getX(), sliderBounds.getY(), sliderBounds.getWidth(), sliderBounds.getHeight(), sliderProportional, startAngle, endAngle, *this);
+    };
+
+    private:
+        CustomLookAndFeel customLnf;
 };
 
 struct CustomVerticalSlider : juce::Slider
